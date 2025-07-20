cccec64b402c141f0fe133741011f986096935b4
Raghav8690 <rajrahul32686@gmail.com>
2025-07-20 17:06:22 +0530

    fetch images from supabase

--- backend/database/supabase_utils.py
+++ backend/database/supabase_utils.py
@@ -1,4 +1,25 @@
 import requests
-from config import supabase
+from config import supabase,SUPABASE_BUCKET1
+from fastapi import HTTPException,FastAPI
 
-def get_data_from_supabase():
+app = FastAPI()
+
+@app.get("/images")
+def get_images_from_supabase():
+    result = supabase.storage.from_(SUPABASE_BUCKET1).list()
+    
+    try:
+        file = result[0]
+        file_name = file["name"]
+        image = supabase.storage.from_('android').download(file_name)
+
+        return image
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching images: {str(e)}")
+
+
+    
+    
+        
+
+        
