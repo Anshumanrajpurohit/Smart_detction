01ed78c2a443271b2d83f0d20f0d25309e81300e
Crazzy <anshumanawm461@gmail.com>
2025-07-31 17:28:17 +0530

    backend changes

--- backend/main.py
+++ backend/main.py
@@ -4,13 +4,13 @@ from routers.process_image import router as supabase_router
 # idhar routs wali file ko load ka and call main funtion
 app = FastAPI()
 
-# @app.get("/")
-# def index():
-#     return {"msg": "Welcome to the Supabase Image API"}
-
 @app.get("/")
-def read_root():
-    return {"message": "Docker is working with FastAPI + DeepFace!"}
+def index():
+    return {"msg": "Welcome to the Supabase Image API"}
 
-# app.include_router(supabase_router, prefix="/api", tags=["Supabase Images"])
+# @app.get("/")
+# def read_root():
+#     return {"message": "Docker is working with FastAPI + DeepFace!"}
+
+app.include_router(supabase_router, prefix="/api", tags=["Supabase Images"])
 
